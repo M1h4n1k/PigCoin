bdd9ea73923bfc650bceceef0c90430d07f48895
M1h4n1k <mihanik@internet.ru>
2024-06-06 21:21:04 +0300

    boosters page implemented

---
+++ frontend/src/components/Booster.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex w-full items-center justify-between rounded-2xl">
+    <div class="flex items-center justify-center">
+      <div
+        class="flex h-[70px] w-[70px] items-center justify-center rounded-xl border p-2"
+      >
+        <img height="50" width="50" src="/pig.png" alt="refill" />
+      </div>
+      <div class="ml-2">
+        <p class="text-xl">Pig</p>
+        <p class="text-gray-600">🐽512,000</p>
+      </div>
+    </div>
+
+    <svg height="25px" width="25px" viewBox="0 0 100 100">
+      <path
+        d="M40.0 21.7L65.3 47.0Q66.7 48.3 66.7 49.8Q66.7 51.3 65.7 52.7L65.7 52.7L40.3 78Q39.3 79 37.7 79.2Q36.0 79.3 34.7 78.2Q33.3 77 33.3 75.3Q33.3 73.7 34.3 72.3L34.3 72.3L56.7 50.0L34.7 28.0Q33.3 27.0 33.3 25.3Q33.3 23.7 34.3 22.3L34.3 22.3L34.7 22.0Q35.7 21.0 37.2 20.8Q38.7 20.7 40.0 21.7L40.0 21.7Z"
+      ></path>
+    </svg>
+  </div>
+</template>
+
+<style scoped></style>
--- frontend/src/views/Boosts.vue
+++ frontend/src/views/Boosts.vue
@@ -1,5 +1,47 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Booster from "@/components/Booster.vue";
+</script>
 
-<template>Boosters</template>
+<template>
+  <div class="px-5 py-2">
+    <div class="text-center">
+      <p class="text-lg">Balance</p>
+      <p class="text-4xl font-medium">🐽132 456</p>
+    </div>
+    <div class="mt-5">
+      <h3 class="text-xl">Free daily boosters</h3>
+      <div class="mt-2 flex justify-around gap-4">
+        <div
+          class="flex w-1/2 items-center justify-between rounded-2xl border py-2 pl-4 pr-2"
+        >
+          <div>
+            <p class="text-xl">Pig</p>
+            <p class="text-gray-600">2/3 available</p>
+          </div>
+          <img height="50" width="50" src="/pig.png" alt="refill" />
+        </div>
+
+        <div
+          class="flex w-1/2 items-center justify-between rounded-2xl border py-2 pl-4 pr-2"
+        >
+          <div>
+            <p class="text-xl">Refill</p>
+            <p class="text-gray-600">2/3 available</p>
+          </div>
+          <img height="60" width="60" src="/soapRefill.jpg" alt="refill" />
+        </div>
+      </div>
+    </div>
+
+    <div class="mt-5">
+      <h3 class="text-xl">Boosters</h3>
+      <div
+        class="mt-2 flex flex-col justify-around gap-4 rounded-xl border p-5"
+      >
+        <Booster v-for="i in 5" :key="i" />
+      </div>
+    </div>
+  </div>
+</template>
 
 <style scoped></style>
