c3791d5f62bb0c982c9063e44e0bb9e7011d4f81
M1h4n1k <mihanik@internet.ru>
2024-07-24 21:42:01 +0300

    refactored icons

---
+++ frontend/src/components/AlertToast.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import IconAlertInfo from "@/components/IconAlertInfo.vue";
+import IconAlertError from "@/components/IconAlertError.vue";
+import { useAlertStore } from "@/store.ts";
+
+const alertStore = useAlertStore();
+</script>
+
+<template>
+  <div
+    class="pointer-events-none fixed top-3 z-50 flex w-full items-center justify-center px-2 opacity-0 transition-opacity duration-300"
+    :class="{
+      '!opacity-100': alertStore.isDisplayed,
+    }"
+  >
+    <div
+      class="flex h-[45px] w-full select-none items-center rounded-xl bg-gray-800 pl-2"
+    >
+      <IconAlertError v-if="alertStore.type === 'error'" />
+      <IconAlertInfo class="ml-[10px]" v-else-if="alertStore.type === 'info'" />
+      <span class="ml-[5px] text-white">{{ alertStore.message }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
