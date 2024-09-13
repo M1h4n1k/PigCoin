667b2cfc5d75877f3c89e1a9bcb64edd50fb1957
M1h4n1k <mihanik@internet.ru>
2024-09-13 13:09:48 +0300

    fix 422 errors

--- frontend/src/components/HomePorkNose.vue
+++ frontend/src/components/HomePorkNose.vue
@@ -133,10 +133,18 @@ const collectCoinsBatch = () => {
       "Content-Type": "application/json",
     },
   })
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
     .then((data) => {
       userStore.user = data;
       coinsCollectedBatch.value = 0;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
     });
 };
 
